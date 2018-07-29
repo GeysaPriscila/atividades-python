9a11a9f13c1cb71b788761d34d9a775757df5298
Geysa Priscila <geysapriscilasz@gmail.com>
2018-07-29 17:00:02 -0300

    Tempo de Atraso

---
+++ tempo-de-atraso.py
@@ -0,0 +1,25 @@
+'''
+Bino tem que ir para uma reunião e não sabe se vai conseguir chegar a tempo para a reunião.
+Ele leva exatamente 45 minutos para sair da sua casa e chegar no local da reunião.
+Faça um programa que receba quatro inteiros Ha, Ma, Hr e Mr. Ha e Ma representam a hora e o minuto atual.
+Hr e Mr representam a hora e o minuto que a reunião começa.
+É garantido que a reunião de Bino vai acontecer ainda no mesmo dia.
+
+Entrada
+A entrada consiste de quatro linhas.
+A primeira linha contém o valor de Ha.
+A segunda linha contém o valor de Ma.
+A terceira linha contém o valor de Hr.
+A quarta linha contém o valor de Mr.
+
+Saída
+A saída consiste de uma única linha.
+Caso Bino consiga chegar na reunião até o momento que ela começa, imprima a mensagem "Sucesso".
+Caso Bino não consiga chegar na reunião até o momento que ela começa, imprima "Atrasado X", sendo X igual ao tempo que ele vai chegar atrasado.
+'''
+
+Ha = int(input())
+Ma = int(input())
+Hr = int(input())
+Mr = int(input())
+
