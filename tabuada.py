c5e9353f88851a0ba840998a592d91e8b1dcb353
Geysa Priscila <geysapriscilasz@gmail.com>
2018-07-29 17:42:16 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,16 @@
+"""
+Bino está aprendendo a multiplicar números e gostaria da sua ajudar para apresentar a tabuada de um determinado valor inteiro X.
+
+Entrada
+A entrada consiste em uma única linha contendo o valor inteiro X.
+
+Saída
+A saída consiste de 10 linhas.
+A i-éssima linha constem a mensagem "x * i = valor",
+onde x é o valor de X, i representa a o identificar da linha e o valor é o valor da múltiplicação de X∗i
+"""
+
+x = int(input())
+
+
+
