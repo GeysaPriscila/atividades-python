907fe797c4e548673a95aba533b60994dc8997be
Geysa Priscila <geysapriscilasz@gmail.com>
2018-07-29 17:13:47 -0300

    Repetir X Vezes

---
+++ repetir-x-vezes.py
@@ -0,0 +1,16 @@
+"""
+Faça um programa que leia um inteiro X e imprima X vezes a mensagem "NepsAcademy eh Sucesso".
+
+Entrada
+A entrada consiste de uma linha contendo um inteiro X.
+
+Saída
+A saída consiste de X linhas, cada uma contendo a mensagem indicada no enunciado.
+"""
+
+x = int(input())
+i = 0
+
+while i < x:
+    print ("NepsAcademy eh Sucesso")
+    i+=1
